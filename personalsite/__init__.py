854ca951d50fd3fcf55f885832e35603060e979d
ndjenkins85 <nick_jenkins@outlook.com>
2021-09-04 15:01:38 -0400

    0.2.1

--- personalsite/__init__.py
+++ personalsite/__init__.py
@@ -45,6 +45,7 @@ import personalsite.views  # noqa: E402
 # Git tagging is required in addition to these changes
 # See CONTRIBUTING.md for more info
 
-__version__ = "0.2.0"  # Nox tests passing
+__version__ = "0.2.1"  # Bugfix fix views not importing, nox problems, tags on article template
+# "0.2.0"  # Nox tests passing
 # "0.1.1"  # Add infrastructure tooling
 # "0.1.0"  # Initial commit
